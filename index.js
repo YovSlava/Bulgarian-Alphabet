0c9c66b1fa6d7eeff12660f99dae1698a6ab9c45
YovSlava <50184380+YovSlava@users.noreply.github.com>
2020-08-06 21:57:22 +0300

    button fixed
    button sound when keydown fixed, audio files added for buttons a-b-v only

--- index.js
+++ index.js
@@ -3,64 +3,63 @@ var buttonLength = document.querySelectorAll(".letter").length;
 //
 
 for (i = 0; i < buttonLength; i++) {
- document.querySelectorAll(".letter")[i].addEventListener("click", function () {
-  var switchButtonHTML = this.innerHTML;
+  document
+    .querySelectorAll(".letter")
+    [i].addEventListener("click", function () {
+      var switchButtonHTML = this.innerHTML;
 
-  makeSound(buttonInnerHTML);
+      makeSound(buttonInnerHTML);
 
-  buttonAnimation(buttonInnerHTML);
- });
+      buttonAnimation(buttonInnerHTML);
+    });
 }
 
 document.addEventListener("keypress", function (event) {
- makeSound(event.key);
- buttonAnimation(event.key);
+  makeSound(event.key);
+  buttonAnimation(event.key);
 });
-
-
 
 //
 
 function makeSound(key) {
- switch (key) {
-  case "a":
-   var am4a = new Audio('audio/a.m4a');
-   tom1.play();
-   break;
+  switch (key) {
+    case "a":
+      var am4a = new Audio("audio/a.mp3");
+      am4a.play();
+      break;
 
-  case "b":
-   var tom1 = new Audio('audio/tom-1.mp3');
-   tom1.play();
-   break;
+    case "b":
+      var tom1 = new Audio("audio/tom-1.mp3");
+      tom1.play();
+      break;
 
-  case "v":
-   var tom1 = new Audio('audio/tom-1.mp3');
-   tom1.play();
-   break;
+    case "v":
+      var tom1 = new Audio("audio/tom-1.mp3");
+      tom1.play();
+      break;
 
-  case "g":
-   var tom1 = new Audio('audio/tom-1.mp3');
-   tom1.play();
-   break;
+    case "g":
+      var tom1 = new Audio("audio/tom-1.mp3");
+      tom1.play();
+      break;
 
-  case "d":
-   var tom1 = new Audio('audio/tom-1.mp3');
-   tom1.play();
-   break;
+    case "d":
+      var tom1 = new Audio("audio/tom-1.mp3");
+      tom1.play();
+      break;
 
+    case "e":
+      var tom1 = new Audio("audio/tom-1.mp3");
+      tom1.play();
+      break;
 
-  case "e":
-   var tom1 = new Audio('audio/tom-1.mp3');
-   tom1.play();
-   break;
+    case "j":
+      var tom1 = new Audio("audio/tom-1.mp3");
+      tom1.play();
+      break;
 
-  case "j":
-   var tom1 = new Audio('audio/tom-1.mp3');
-   tom1.play();
-   break;
-
-  default:
-   console.log(switchButtonHTML);
-   break;
- }
+    default:
+      console.log(switchButtonHTML);
+      break;
+  }
 }
